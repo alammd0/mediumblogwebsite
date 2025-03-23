b90a43fb49b09753bd38c5bdb01c962845922734
Md Khalid Alam <sonualam96314@gmail.com>
2025-03-23 14:40:39 +0530

    some addition

---
+++ src/routes/user.ts
@@ -0,0 +1,91 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from "hono/jwt";
+
+
+export const userRouter = new Hono<{
+    Bindings: {
+        DATABASE_URL: string
+        JWT_SECRET: string
+    }
+}>();
+
+
+userRouter.post("/signup", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    try {
+        const body = await c.req.json();
+        const user = await prisma.user.create({
+            data: {
+                username: body.username,
+                email: body.email,
+                name: body.name,
+                password: body.password
+            }
+        });
+
+        const token = await sign({
+            id: user.id
+        }, c.env.JWT_SECRET)
+
+
+        return c.json({
+            message: "User created successfully",
+            data: user,
+            jwt: token
+        }, 200)
+    }
+
+    catch (err) {
+        return c.json({
+            message: "User not created",
+            error: err
+        }, 400)
+    }
+})
+
+
+userRouter.post("/signin", async (c) => {
+    const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
+
+    try {
+
+        const body = await c.req.json();
+        const user = await prisma.user.findFirst({
+            where: {
+                email: body.email,
+                password: body.password
+            }
+        })
+
+
+        if (!user) {
+            return c.json({
+                message: "User not found",
+            }, 403)
+        }
+
+        const token = await sign({
+            id: user.id
+        }, c.env.JWT_SECRET)
+
+
+        return c.json({
+            message: "User logged in successfully",
+            data: user,
+            jwt: token
+        }, 200)
+    }
+    catch (err) {
+        return c.json({
+            message: "User not logged in",
+            error: err
+        }, 400)
+    }
+})
